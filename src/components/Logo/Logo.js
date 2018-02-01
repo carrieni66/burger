3d957e8bfd8c2bc25c0e6dce9f4b451d2af5e321
carrie <ui.carrie1@gmail.com>
2018-02-01 16:09:30 -0500

    second

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,6 +2,7 @@ import React from 'react';
 import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css';
 
+
 const logo = (props) => ( 
     <div className = { classes.Logo }
     style = {
